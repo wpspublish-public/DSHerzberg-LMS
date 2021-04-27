ca68df80416cda2b710549c125b25d1a21968530
David Herzberg <dherzberg@wpspublish.com>
2021-04-26 21:58:03 -0700

    solve input_clean

--- CODE/reprex1.R
+++ CODE/reprex1.R
@@ -1,8 +1,30 @@
+library(here)
 library(tidyverse)
 
 input <- read_csv(url(
   "https://raw.github.com/wpspublish/DSHerzberg-LMS/master/INPUT-FILES/reprex-input.csv"
 ))
 
+input_clean <- input$q1 %>% 
+  str_replace_all( " [|] Excellent", "")
 
+input_clean2 <- input %>% 
+  mutate(across(
+    everything(),
+    ~ str_replace(., " [|] Excellent", "")
+  ))
+      
+input_clean3 <- input %>%
+  mutate(across(everything(),
+                ~ str_replace_all(
+                  .,
+                  c(
+                    " [|] Excellent" = "",
+                    " [|] Above Average" = "",
+                    " [|] Average" = "",
+                    " [|] Below average" = "",
+                    " [|] Poor" = ""
+                  )
+                )))
 
+write_csv(input_clean3, here("test.csv"))
--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,80 +1,30 @@
-vec <- input$`Access/Setting/Overall Experience`
-# str_replace_all(vec, "[|]", "")
-vec2 <- str_replace_all(vec, "Excellent, ", "")
-vec3 <- str_replace_all(vec, c("Excellent, " = "", "Above average, " = "", 
-                               "Average, " = "", "Below average, " = "", 
-                               "Poor, " = ""))
-vec3[1]
+library(here)
+library(tidyverse)
 
+input <- read_csv(url(
+  "https://raw.github.com/wpspublish/DSHerzberg-LMS/master/INPUT-FILES/reprex-input.csv"
+))
 
+input_clean <- input$q1 %>% 
+  str_replace_all( " [|] Excellent", "")
 
+input_clean2 <- input %>% 
+  mutate(across(
+    everything(),
+    ~ str_replace(., " [|] Excellent", "")
+  ))
 
-see <- function(rx) str_view_all("abc ABC 123\t.!|?\\(){}\n", rx)
-see("[[:space:]]")
+input_clean3 <- input %>%
+  mutate(across(everything(),
+                ~ str_replace_all(
+                  .,
+                  c(
+                    " [|] Excellent" = "",
+                    " [|] Above average" = "",
+                    " [|] Average" = "",
+                    " [|] Below average" = "",
+                    " [|] Poor" = ""
+                  )
+                )))
 
-super_sub_rating_anchors <- c("Excellent, ", "Above average, ", 
-                              "Average, ", "Below average, ", "Poor, ")
-
-c("X" = "", "Y" = "-")
-
-vec0 <- str_c(super_sub_rating_anchors, " = ''")
-
-vec_matches <- c("Excellent, " = "", "Above average, " = "", 
-                 "Average, " = "", "Below average, " = "", "Poor, " = "")
-
-df1 <- str_replace_all(df_super_sub_cols, c("Excellent, " = "", "Above average, " = "", 
-                               "Average, " = "", "Below average, " = "", 
-                               "Poor, " = ""))
-
-vec_clean <- list_sub_q_names[[1]] %>%
-           str_replace_all(
-             c(
-               "_Excellent,_" = "",
-               "_Above average,_" = "",
-               "_Average,_" = "",
-               "_Below average,_" = "",
-               "_Poor,_" = ""
-             )
-           )
-
-list_sub_q_names <- map(
-  list_super_sub_cols,
-  ~ .x %>%
-    select(contains("q")) %>%
-    filter(row_number() == 1) %>%
-    as.character() %>%
-    str_replace_all(" ", "_")
-)
-
-df_clean <- df_super_sub_cols$`Access/Setting/Overall Experience` %>% 
-  str_replace_all(
-    c(
-      " | Excellent" = "",
-      " | Above Average" = "",
-      " | Average" = "",
-      " | Below average" = "",
-      " | Poor" = ""
-    )  
-  )
-
-df_super_sub_cols$`Access/Setting/Overall Experience` <-  
-  str_replace_all(df_super_sub_cols$`Access/Setting/Overall Experience`, 
-    c(
-      " | Excellent" = "",
-      " | Above Average" = "",
-      " | Average" = "",
-      " | Below average" = "",
-      " | Poor" = ""
-    )  
-  )
-
-df_clean <- df_super_sub_cols$`Access/Setting/Overall Experience` %>% 
-  str_replace_all( " | Excellent", "")
- 
-df_super_sub_cols$`Access/Setting/Overall Experience` <- 
-  str_replace_all(df_super_sub_cols$`Access/Setting/Overall Experience`, ',', 'AA') %>% 
-  unnest()
-
-df_super_sub_cols$`Access/Setting/Overall Experience` <- 
-  gsub(',', 'AA', df_super_sub_cols$`Access/Setting/Overall Experience`)
-
+write_csv(input_clean3, here("test.csv"))
